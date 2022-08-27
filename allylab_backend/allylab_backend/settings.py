753d3d28926c69af14c985d0ba5fd6b28f989319
Rebecca Mansell <rebeccakatemansell@gmail.com>
2022-08-27 12:08:39 +1000

    removed weird thing

--- allylab_backend/allylab_backend/settings.py
+++ allylab_backend/allylab_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-i_5mn2u7pt27x&%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG') != 'False'
 
-ALLOWED_HOSTS = ['*', 'allylab-backend.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
