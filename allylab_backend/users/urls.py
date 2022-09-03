2e8426a87a75aa769b4a00ee7c859bee32a564d0
Rebecca Mansell <rebeccakatemansell@gmail.com>
2022-09-03 12:21:41 +1000

    skills many to many working, commit to skills

--- allylab_backend/users/urls.py
+++ allylab_backend/users/urls.py
@@ -3,9 +3,14 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
+<<<<<<< HEAD
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
     path('skills/', views.SkillList.as_view()),
+=======
+    path('users/', views.CustomUserList.as_view()),
+    path('users/<int:pk>/', views.CustomUserDetail.as_view()),
+>>>>>>> d3a95034cb3d37350d48bf3fa6a0d41cb77bcb7f
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
